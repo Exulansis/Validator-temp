96a39be14949ae722b65a014633de768689125de
Exulansis <rusu.eugeniu@gmx.de>
2018-11-23 10:52:06 +0100

    Update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const instantiateIdentity = require('./validator')
 const { JolocomLib } = require('jolocom-lib')
 
-const setupApp = (iw) => {
+const setupApp = iw => {
   const app = express()
 
   app.use((req, res, next) => {
@@ -15,16 +15,46 @@ const setupApp = (iw) => {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
 
+  app.get('/authenticate:callbackURL', async (req, res) => {
+    const { callbackURL } = req.params
+    const credentialRequest = await iw.create.interactionTokens.request.share(
+      {
+        callbackURL,
+        credentialRequirements: [
+          {
+            type: ['Credential', 'ProofOfEmailCredential'],
+            constraints: []
+          },
+          {
+            type: ['Credential', 'ProofOfNameCredential'],
+            constraints: []
+          }
+        ]
+      },
+      'secret'
+    )
+
+    console.log('New authentication request generated')
+    res.send(credentialRequest.encode())
+  })
+
   app.post('/authenticate', async (req, res) => {
     const { token } = req.body
     const credentialResponse = await JolocomLib.parse.interactionToken.fromJWT(token)
-  
-    /** Ensure request and response match, will check the nonce, audience, and signature. Will check the callbackURL too in next
-      *  minor release.
-      */
-  
-    await iw.validateJWT(credentialResponse, null)
-    console.log(credentialResponse)
+
+    /**
+     *  Ensure request and response match, will check the nonce, audience, and signature. Will check the callbackURL too in next
+     *  minor release.
+     */
+
+    try {
+      await iw.validateJWT(credentialResponse)
+      console.log('Signature validated')
+      console.log('request from', credentialResponse.issuer)
+      res.send('OK')
+    } catch (err) {
+      res.sendStatus(401)
+    }
   })
 
   const port = 8000
